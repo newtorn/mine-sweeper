ae65b1ea50cbca1126c471f4c61682e9ba1e6d5b
newtorn <codertorn@gmail.com>
2019-05-05 20:35:41 +0800

    finshed basic window forms

---
+++ mine-sweeper/Loadscreen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace mine_sweeper
+{
+    public partial class Loadscreen : Form
+    {
+        public Loadscreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
