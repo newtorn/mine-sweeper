98ba4f394d3fe1470fb7e9d09fb04d780d269630
newtorn <codertorn@gamil.com>
2019-05-10 20:45:12 +0800

    updated forms

--- mine-sweeper/Game.Designer.cs
+++ mine-sweeper/Game.Designer.cs
@@ -31,6 +31,10 @@
             this.components = new System.ComponentModel.Container();
             this.timer = new System.Windows.Forms.Timer(this.components);
             this.ControlBar = new System.Windows.Forms.Panel();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.ScaleButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ControlBar.SuspendLayout();
             this.SuspendLayout();
             // 
             // timer
@@ -40,18 +44,57 @@
             // 
             // ControlBar
             // 
-            this.ControlBar.BackColor = System.Drawing.Color.Gray;
+            this.ControlBar.BackColor = System.Drawing.Color.LightGray;
+            this.ControlBar.Controls.Add(this.button1);
+            this.ControlBar.Controls.Add(this.ScaleButton);
+            this.ControlBar.Controls.Add(this.closeButton);
             this.ControlBar.Location = new System.Drawing.Point(0, 0);
             this.ControlBar.Name = "ControlBar";
-            this.ControlBar.Size = new System.Drawing.Size(620, 20);
+            this.ControlBar.Size = new System.Drawing.Size(620, 24);
             this.ControlBar.TabIndex = 0;
+            // 
+            // closeButton
+            // 
+            this.closeButton.BackColor = System.Drawing.Color.OrangeRed;
+            this.closeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.closeButton.Location = new System.Drawing.Point(15, 5);
+            this.closeButton.Margin = new System.Windows.Forms.Padding(0);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(14, 14);
+            this.closeButton.TabIndex = 0;
+            this.closeButton.UseVisualStyleBackColor = false;
+            // 
+            // ScaleButton
+            // 
+            this.ScaleButton.BackColor = System.Drawing.Color.Gold;
+            this.ScaleButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ScaleButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ScaleButton.Location = new System.Drawing.Point(35, 5);
+            this.ScaleButton.Margin = new System.Windows.Forms.Padding(0);
+            this.ScaleButton.Name = "ScaleButton";
+            this.ScaleButton.Size = new System.Drawing.Size(14, 14);
+            this.ScaleButton.TabIndex = 1;
+            this.ScaleButton.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SpringGreen;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(55, 5);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(14, 14);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // Game
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.LightGray;
-            this.ClientSize = new System.Drawing.Size(614, 374);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(620, 374);
             this.ControlBox = false;
             this.Controls.Add(this.ControlBar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -61,6 +104,7 @@
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.exit);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.redraw);
             this.Resize += new System.EventHandler(this.resize);
+            this.ControlBar.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -69,5 +113,8 @@
 
         private System.Windows.Forms.Timer timer;
         private System.Windows.Forms.Panel ControlBar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button ScaleButton;
+        private System.Windows.Forms.Button closeButton;
     }
 }
